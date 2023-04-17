d45389b1c1e71814e005fbdbb5c7ddedf4fc6813
Ahmed Mounir <Ahmedmounir22.com>
2023-04-17 02:41:54 -0700

    libmy

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string for any of a set bytes
+ *
+ * @s: string
+ * @accept: string to match
+ *
+ * Return: pointer to the byte in a that matches one of the bytes in accept
+ * of NULL if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a, b;
+	char *p;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		b = 0;
+		while (accept[b] != '\0')
+		{
+			if (accept[b] == s[a])
+			{
+				p = &s[a];
+				return (p);
+			}
+			b++;
+		}
+		a++;
+	}
+
+	return (0);
+}
