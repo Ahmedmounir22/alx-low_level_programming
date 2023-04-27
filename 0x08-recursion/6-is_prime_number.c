7797f90f4eefadc7786d23b49de38d2c19d32fdd
Ahmed Mounir <Ahmedmounir22.com>
2023-04-26 20:01:29 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -29,7 +29,7 @@ int check_prime(int n, int other)
 	if (other >= n && n > 1)
 		return (1);
 	else if (n % other == 0 || n <= 1)
-		return (1);
+		return (0);
 	else
 		return (check_prime(n, other + 1));
 }
