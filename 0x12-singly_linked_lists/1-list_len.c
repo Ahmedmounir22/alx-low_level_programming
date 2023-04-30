f1b4efa246cc452cdd80288896f045323059ffe4
Ahmed Mounir <Ahmedmounir22.com>
2023-04-30 06:40:41 -0700

    a

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,4 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
  * list_len - returns the number of elements in a linked list_t list
@@ -8,8 +8,9 @@ nclude "lists.h"
 
 size_t list_len(const list_t *h)
 {
-	size_t count = 0; /* counter variable to keep track of number of nodes */
-
+	size_t count = 0; 
+	
+	/* counter variable to keep track of number of nodes */
 	while (h != NULL)
 	{
 		count++;
