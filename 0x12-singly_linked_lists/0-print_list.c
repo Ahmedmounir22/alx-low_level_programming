0d0f31db72a6a956131bce6baae0a6faf240257e
Ahmed Mounir <Ahmedmounir22.com>
2023-04-30 06:29:33 -0700

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- *  * print_list - prints all the elements of a list_t list
- *   * @h: head of linked list
- *    * Return: number of nodes in linked list
- *     */
+ * print_list - prints all the elements of a list_t list
+ * @h: head of linked list
+ * Return: number of nodes in linked list
+ */
 
 size_t print_list(const list_t *h)
 {
