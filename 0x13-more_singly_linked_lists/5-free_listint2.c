c4479039317c9718fa85d89369e272321b219cad
Ahmed Mounir <ahmedmouni82@gmail.com>
2023-05-02 17:55:04 -0700

    a

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,9 +3,11 @@
 /**
  * free_listint - Frees a listint_t list.
  * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: void
 */
 
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *node, *temp;
 
