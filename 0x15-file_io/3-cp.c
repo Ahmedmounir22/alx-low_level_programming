7365ea52a8634eed45354f03b7d6cea54375311d
Ahmed Mounir <ahmedmouni82@gmail.com>
2023-07-10 18:13:09 -0700

    FILE I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,58 +2,48 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 #define BUFFER_SIZE 1024
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file NAME_OF_THE_FILE %s\n"
+#define ERR_NOWRITE "Error: Can't write to NAME_OF_THE_FILE %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd FD_VALUE %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | s_IROTH)
 
 /**
  * main - the entry point of the cp program
- * @argc: the number of command-line arguments
- * @argv: an array of command-line argument strings
+ * @ac: the number of command-line arguments
+ * @av: an array of command-line argument strings
  *
  * Return: 0 on success, or non-zero on failure
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	int fd_from, fd_to, bytes_read, bytes_written, close_status;
-	char buffer[BUFFER_SIZE];
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int from_fd = 0, to_fd = 0;
+	ssize_t b;
+	char buf[READ_BUF_SIZE];
 
-	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	if (ac != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+	from_fd = open(av[1], O_RDONLY);
+	if (from_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSION);
+	if (to_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read
-				from file %s\n", argv[1]), exit(98);
+	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
+		if (write(to_fd, buf, b) != b)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	if (b == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1], exit(98));
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (fd_to == -1)
-		close(fd_from), dprintf(STDERR_FILENO, "Error: Can't
-				write to %s\n", argv[2]), exit(99);
+	from_fd = close(from_fd);
+	to_fd = close(to_fd);
+	if (from_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	if (to_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-			close(fd_from), close(fd_to), dprintf(STDERR_FILENO, "Error: Can't
-					write to %s\n", argv[2]), exit(99);
-	}
-
-	if (bytes_read == -1)
-		close(fd_from), close(fd_to), dprintf(STDERR_FILENO, "Error: Can't
-				read from file %s\n", argv[1]), exit(98);
-
-	close_status = close(fd_from);
-	if (close_status == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-
-	close_status = close(fd_to);
-	if (close_status == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-
-	return (0);
+	return (EXIT_SUCCESS);
 }
